fa8238dc8323a03142d4a6012a6f6ab8934aa6e2
Gladiale <the_Garden_of_sinners@protonmail.com>
2024-11-05 17:57:35 +0800

    🐛 rotateの不具合を修正

--- src/components/EffectImage/EffectImage.tsx
+++ src/components/EffectImage/EffectImage.tsx
@@ -31,49 +31,61 @@ const EffectImage = () => {
     }
   };
 
-  let imgWidth: "auto" | "100%", imgHeight: "auto" | "100%";
+  let imgWidth: "auto" | "100%",
+    imgHeight: "auto" | "100%",
+    divHeight: "auto" | "100%" | "120%";
   switch (effectState.imageEF.size) {
     case "none":
       imgWidth = "auto";
       imgHeight = "auto";
+      divHeight = "auto";
       break;
     case "cover":
       imgWidth = "100%";
       imgHeight = "100%";
+      divHeight = "100%";
       break;
     default:
       effectState.imageEF.maxHeightFull
-        ? ((imgWidth = "auto"), (imgHeight = "100%"))
-        : ((imgWidth = "100%"), (imgHeight = "auto"));
+        ? ((imgWidth = "auto"), (imgHeight = "100%"), (divHeight = "100%"))
+        : ((imgWidth = "100%"), (imgHeight = "100%"), (divHeight = "120%"));
   }
 
   return (
-    <img
-      className={`${styles["effect-img"]} ${isEditMode ? styles.isEditing : ""}
+    <div
+      className={`${styles["effect-box"]}
       ${effectState.imageEF.position === "top-left" && styles.topLeft}
       ${effectState.imageEF.position === "top-right" && styles.topRight}
       ${effectState.imageEF.position === "bottom-left" && styles.bottomLeft}
-      ${effectState.imageEF.position === "bottom-right" && styles.bottomRight}
-      `}
-      src={`/effect/${mediaState.folder.effect[1]}/${mediaState.file.effectFile[1]}`}
+      ${effectState.imageEF.position === "bottom-right" && styles.bottomRight}`}
       style={{
+        width: imgWidth,
+        height: divHeight,
+        // maxHeight: effectState.imageEF.size === "contain" ? "120%" : undefined,
+        scale: String(imageScale),
+        transform: `translate(${imagePosition.x}px, ${imagePosition.y}px)`,
         mixBlendMode: effectState.imageEF.activeBlend
           ? effectState.imageEF.blendKind
           : undefined,
-        objectFit: effectState.imageEF.size,
-        height: imgHeight,
-        width: imgWidth,
-        maxHeight: effectState.imageEF.size === "contain" ? "120%" : undefined,
-        transform: rotateYState.effectRotateY
-          ? `rotateY(180deg) rotate(${imageDeg}deg) translate(${imagePosition.x}px, ${imagePosition.y}px)`
-          : `rotate(${imageDeg}deg) translate(${imagePosition.x}px, ${imagePosition.y}px)`,
-        scale: String(imageScale),
       }}
-      onClick={changeImageDeg}
-      onMouseDown={triggerEditMode}
-      onMouseMove={moveImageDirect}
-      onWheel={changeImageScale}
-    />
+    >
+      <img
+        className={`${styles["effect-img"]} ${isEditMode ? styles.isEditing : ""}`}
+        src={`/effect/${mediaState.folder.effect[1]}/${mediaState.file.effectFile[1]}`}
+        style={{
+          objectFit: effectState.imageEF.size,
+          width: imgWidth,
+          height: imgHeight,
+          transform: rotateYState.effectRotateY
+            ? `rotateY(180deg) rotate(${imageDeg}deg)`
+            : `rotate(${imageDeg}deg)`,
+        }}
+        onClick={changeImageDeg}
+        onMouseDown={triggerEditMode}
+        onMouseMove={moveImageDirect}
+        onWheel={changeImageScale}
+      />
+    </div>
   );
 };
 
