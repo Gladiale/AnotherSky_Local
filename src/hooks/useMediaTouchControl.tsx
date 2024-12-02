47e122513f93e6bead6e2107a7b42271c1c63e54
Gladiale <the_Garden_of_sinners@protonmail.com>
2024-12-02 19:43:00 +0800

    🐛 バッグを修正

--- src/hooks/useMediaTouchControl.tsx
+++ src/hooks/useMediaTouchControl.tsx
@@ -12,16 +12,22 @@ const useMediaTouchControl = ({ target }: ParamsType) => {
 
   const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
     e.stopPropagation();
-    // 最初のタッチポイントを取得
-    const touch = e.touches[0];
+    // ターゲットエレメントの中央座標を取得
+    const targetData = e.currentTarget.getBoundingClientRect();
     setStartPoint({
-      x: touch.clientX,
-      y: touch.clientY,
+      x:
+        targetData.x +
+        targetData.width / 2 -
+        mediaState[target].position.x * mediaState[target].scale,
+      y:
+        targetData.y +
+        targetData.height / 2 -
+        mediaState[target].position.y * mediaState[target].scale,
     });
 
     if (target === "effect" && mediaState.touchMode === "rotateMod") {
       // windowの左右を基準に回転方向を決め
-      const mouseXatWindow = touch.clientX;
+      const mouseXatWindow = e.touches[0].clientX;
       const windowWidth = window.innerWidth;
       setMediaState((prev) => ({
         ...prev,
@@ -41,8 +47,9 @@ const useMediaTouchControl = ({ target }: ParamsType) => {
   const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
     e.stopPropagation();
     const touch = e.touches[0];
-    const transitionX = touch.clientX - startPoint.x;
-    const transitionY = touch.clientY - startPoint.y;
+    const mediaScale = mediaState[target].scale;
+    const transitionX = (touch.clientX - startPoint.x) / mediaScale;
+    const transitionY = (touch.clientY - startPoint.y) / mediaScale;
 
     if (mediaState.touchMode === "positionMode") {
       setMediaState({
