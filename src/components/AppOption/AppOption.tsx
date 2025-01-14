85140e760bb5207201a4c8d4676b2aa4cac64610
Gladiale <the_Garden_of_sinners@protonmail.com>
2025-01-14 19:30:05 +0800

    ♻️ リファクタリング

--- src/components/AppOption/AppOption.tsx
+++ src/components/AppOption/AppOption.tsx
@@ -1,15 +1,17 @@
 import styles from "./AppOption.module.css";
 import { useEffect, useState } from "react";
-import { GiDelighted } from "react-icons/gi";
+import { GiDelighted, GiForwardField } from "react-icons/gi";
 import { useAppOption } from "../../context/AppOptionContext/AppOptionContext";
 // components
 import Corner from "../Common/Corner";
 import IconDefault from "../Common/IconDefault";
 import AppOptionContent1st from "./AppOptionContent1st";
+import AppOptionContent2nd from "./AppOptionContent2nd";
 
 const AppOption = () => {
   const { appOption, saveStorageData } = useAppOption();
   const [showPanel, setShowPanel] = useState<boolean>(false);
+  const [isContent2nd, setIsContent2nd] = useState<boolean>(false);
 
   useEffect(() => {
     if (showPanel) {
@@ -20,8 +22,8 @@ const AppOption = () => {
   return (
     <div className={styles["app-option"]}>
       <div
-        className={`${styles["trigger-box"]} 
-          ${appOption.dropShadow.icon && styles.shadow}`}
+        className={`${styles["trigger-button"]} 
+        ${appOption.dropShadow.icon && styles.shadow}`}
       >
         <IconDefault
           children={<GiDelighted />}
@@ -34,7 +36,18 @@ const AppOption = () => {
         style={{ right: showPanel ? "0" : "calc(-100dvh / 3)" }}
       >
         <Corner theme="violet" singleConnerWidth="40%" />
-        <AppOptionContent1st />
+
+        <div
+          className={`${styles["content-switch-button"]} 
+          ${appOption.dropShadow.icon && styles.shadow}`}
+        >
+          <IconDefault
+            children={<GiForwardField />}
+            onClick={() => setIsContent2nd((prev) => !prev)}
+          />
+        </div>
+
+        {isContent2nd ? <AppOptionContent2nd /> : <AppOptionContent1st />}
       </div>
     </div>
   );
---
+++ src/components/AppOption/AppOptionContent2nd.tsx
@@ -0,0 +1,7 @@
+import styles from "./AppOptionContent.module.css";
+
+const AppOptionContent2nd = () => {
+  return <div className={styles["control-box-type-1st"]}></div>;
+};
+
+export default AppOptionContent2nd;
