81bf38149a0100e66b96c6c32835998484b973d3
Gladiale <the_Garden_of_sinners@protonmail.com>
2025-01-15 16:36:15 +0800

    update

--- src/components/AppOption/AppOptionContent2nd.tsx
+++ src/components/AppOption/AppOptionContent2nd.tsx
@@ -1,7 +1,11 @@
 import styles from "./AppOptionContent.module.css";
 
 const AppOptionContent2nd = () => {
-  return <div className={styles["control-box-type-1st"]}></div>;
+  return (
+    <div className={styles["control-box-type-1st"]}>
+      <p style={{ color: "blue" }}>開発中です</p>
+    </div>
+  );
 };
 
 export default AppOptionContent2nd;
