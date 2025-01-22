4cf4d769b23711e7017b3ef187fdbf377dc5c058
Gladiale <the_Garden_of_sinners@protonmail.com>
2025-01-22 17:26:24 +0800

    開発中

---
+++ src/context/OrnamentContext/OrnamentInit.ts
@@ -0,0 +1,6 @@
+type OrnamentInfoType = {
+  // index, url, length
+  backLight: [number, string, number];
+  magicCircle1st: [number, string, number];
+  magicCircle2nd: [number, string, number];
+};
